87fc0a9fd69a7c4a90d3765c4985015476bec8b9
cakirgoz <102021371+cakirgoz@users.noreply.github.com>
2025-01-02 02:47:39 +0300

    Update deneyim.py

--- deneyim.py
+++ deneyim.py
@@ -3,11 +3,10 @@ import pandas as pd
 from datetime import date
 
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Your app code goes here
